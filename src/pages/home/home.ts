61a6ed63167831cf0c1ea88cb1ebb4f0f1d257a3
Tiago Gomes <tiago_gomes07@hotmail.com>
2018-02-04 18:19:05 -0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController,LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { EscolhaPage }  from '../escolha/escolha';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+  public carros;
+
+   constructor(
+     public navCtrl: NavController, 
+     private _http: Http,
+     private _loadingCtrl: LoadingController,
+     private _alertCtrl: AlertController
+    ) {}
+    ngOnInit(){
+   
+    //Loader é componente que sobe um toast com uma mensagem
+    let loader = this._loadingCtrl.create({
+      content:'Buscando novos carros, Aguarde...'
+    });
+    loader.present();
+
+    // A resposta da requisição do servidor
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then( carros => { 
+        loader.dismiss(), 
+        this.carros = carros}, 
+        //Experiencia de tratamento de erro
+      ).catch(err => { 
+          console.log(err);
+          loader.dismiss();
+          this._alertCtrl
+            .create({
+              title : 'Falha na conexão',
+              buttons: [{text:'ok'}],
+              subTitle:'Não foi possivel obter a lista de cargos'
+            }).present();
+
+        }  );
+  //Dentro do aerofunction também foi incluido o metodo dismiss para fechar o toast
+  }
+
+
+}
---
+++ src/pages/lancamento/lancamento.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { DateTime } from 'ionic-angular/components/datetime/datetime';
+
+
+
+@Component({
+    templateUrl:'lancamento.html'
+})
+
+export class LancamentoPage{
+
+    tipos: string[];
+
+    constructor( ){
+
+    }
+
+    
+}
